629fcc766eec45c15e2c5d16b4d9ddd622693075
fg274 <fabianganter@posteo.de>
2025-07-31 21:54:49 +0200

    removed unnessecery command

--- docker-test/build-plot/makePlot.sh
+++ docker-test/build-plot/makePlot.sh
@@ -1,2 +1,1 @@
-python3 /var/local/plot.py -f /var/local/metadata -y 2022 -p /mnt/www/
-python3 /var/local/plot.py -f /mnt/storage/metadata_2 -y 2023 -p /mnt/www/
+python3 /var/local/plot.py -f /var/local/metadata -p /mnt/www/
